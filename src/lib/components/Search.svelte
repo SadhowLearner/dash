eb19d79e2a0ee4299d44fd7c8c4f578074ea4458
SadhowLearner <edufirman159@gmail.com>
2025-11-24 12:00:06 +0700

    Add search input

---
+++ src/lib/components/Search.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+
+
+    let query = $state("")
+    const enterEvent = (e: KeyboardEvent) => {
+        if (e.key === "Enter") {
+            window.open(`https://google.com/search?q=${query}`, "_blank")
+        }
+    }
+</script>
+
+<div class="absolute left-5 bottom-8 flex items-center text-white shadow-lg shadow-slate-700">
+  <input bind:value={query} onkeydown={enterEvent} type="text" class="backdrop-blur-xl border-2 bg-slate-500/20 border-none h-12 w-sm text-white drop-shadow-xs shadow-xl shadow-slate-800   drop-shadow-slate-800 focus:shadow-md focus:border-none rounded-2xl">
+  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide me lucide-search-icon lucide-search -translate-x-10"><path d="m21 21-4.34-4.34"/><circle cx="11" cy="11" r="8"/></svg>
+</div>
+
+<style>
+
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,31 +1,52 @@
 <script lang="ts">
-  import Wallpaper from "$lib/components/Wallpaper.svelte"
-  import Timer from "$lib/components/Timer.svelte";
-  import {timerModalOpen, time} from "$lib/components/store"
-	import { formatTime } from "$lib";
-  import timer from "$lib/assets/timer.svg"
+	import Wallpaper from '$lib/components/Wallpaper.svelte';
+	import Timer from '$lib/components/Timer.svelte';
+	import { timerModalOpen, time } from '$lib/components/store';
+	import { formatTime } from '$lib';
+  import Search from "$lib/components/Search.svelte";
 </script>
 
 <svelte:head>
-  <link href="https://fonts.cdnfonts.com/css/segment7" rel="stylesheet">
+	<link href="https://fonts.cdnfonts.com/css/segment7" rel="stylesheet" />
 </svelte:head>
 
-<div class="min-h-screen relative overflow-hidden">
-  <Wallpaper />
-  <button on:click={() => {timerModalOpen.update(v => !v)
-  }} class="text-white cursor-pointer z-999 absolute top-5 right-5 rounded-2xl mx-3 my-2  hover:drop-shadow-[0_0_12px_currentColor]">
-<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-timer-icon lucide-timer"><line x1="10" x2="14" y1="2" y2="2"/><line x1="12" x2="15" y1="14" y2="11"/><circle cx="12" cy="14" r="8"/></svg>  
-</button>
+<div class="relative min-h-screen overflow-hidden">
+	<Wallpaper />
+	<button
+		on:click={() => {
+			timerModalOpen.update((v) => !v);
+		}}
+		class="absolute top-8 left-8 z-999 cursor-pointer rounded-2xl text-white hover:drop-shadow-[0_0_12px_currentColor]"
+	>
+		<svg
+			xmlns="http://www.w3.org/2000/svg"
+			width="32"
+			height="32"
+			viewBox="0 0 24 24"
+			fill="none"
+			stroke="currentColor"
+			stroke-width="2"
+			stroke-linecap="round"
+			stroke-linejoin="round"
+			class="lucide lucide-timer-icon lucide-timer"
+			><line x1="10" x2="14" y1="2" y2="2" /><line x1="12" x2="15" y1="14" y2="11" /><circle
+				cx="12"
+				cy="14"
+				r="8"
+			/></svg
+		>
+	</button>
+	<Timer />
+	<div
+		on:click={timerModalOpen.update((v) => !v)}
+		class="absolute right-8 bottom-8 m-4 cursor-pointer"
+	>
+		<div class="segment7 text-8xl font-bold text-white drop-shadow-[0_0_20px_currentColor]">
+			{formatTime($time)}
+		</div>
+	</div>
 
-  <Timer />
-  <div on:click={timerModalOpen.update(v => !v)} class="cursor-pointer absolute bottom-8 right-8 m-4">
-     <div class="segment7 text-8xl text-white font-bold drop-shadow-[0_0_20px_currentColor]">
-          {formatTime($time)}
-      </div>
-  </div>
+  <Search />
 
-
-
-  <!-- Overlay gelap untuk readability -->
+	<!-- Overlay gelap untuk readability -->
 </div>
-
